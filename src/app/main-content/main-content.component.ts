54ecfe519b65308cfdeb47935117f64afcf1283b
gfike19 <gfike19@gmail.com>
2025-06-23 11:48:59 -0500

    added component where list of sites will go

---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-content',
+  imports: [],
+  templateUrl: './main-content.component.html',
+  styles: `
+    :host {
+      display: block;
+    }
+  `
+})
+export class MainContentComponent {
+
+}
